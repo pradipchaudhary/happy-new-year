0cfb6ca4e254c64401ffb3384dec12397b4303e3
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-04-07 13:58:21 +0545

    UI created..

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div className='container'>
+			<div className='flex'>
+				<div className='font-big'>13</div>
+				<div className='font-small'>DAYS</div>
+			</div>
+			<div className='flex'>
+				<div className='font-big'>13</div>
+				<div className='font-small'>HOURS</div>
+			</div>
+			<div className='flex'>
+				<div className='font-big'>13</div>
+				<div className='font-small'>MINUTES</div>
+			</div>
+			<div className='flex'>
+				<div className='font-big'>13</div>
+				<div className='font-small'>SECOND</div>
+			</div>
+		</div>
+	);
 };
 
 export default App;
