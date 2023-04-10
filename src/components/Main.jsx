47d6a53d9abcd151dcca8a43f1ef9b66e4f814f7
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-04-10 16:39:56 +0545

    fixed date error issue.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const NEW_YEARS_EVE = Date(2021, 11, 31, 0, 0, 0);
+const NEW_YEARS = new Date(2023, 3, 14, 0, 0, 0);
 const getTimedelta = () => {
 	const now = new Date();
-	const timedelta = NEW_YEARS_EVE.getTime() - now.getTime();
+	const timedelta = NEW_YEARS.getTime() - now.getTime();
+
 	// console.log(timedelta);
 	// Diff in days
 	const days = Math.floor(timedelta / (24 * 60 * 60 * 1000));
+
 	// Diff in hours
 	const hours = Math.floor(
 		(timedelta % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000)
@@ -26,23 +28,36 @@ const getTimedelta = () => {
 };
 
 const Main = () => {
+	const [countdown, setCountdown] = useState(getTimedelta);
+	// const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			const timedelta = getTimedelta();
+			setCountdown(timedelta);
+		}, 1000);
+		return () => clearInterval(interval);
+	});
+	// Loading function
+	// if (loading) {
+	// 	return <h1>Loading...</h1>;
+	// }
 	return (
 		<main>
 			<div className='container'>
 				<div className='flex'>
-					<div className='font-big'>13</div>
+					<div className='font-big'>{countdown.days}</div>
 					<div className='font-small'>DAYS</div>
 				</div>
 				<div className='flex'>
-					<div className='font-big'>13</div>
+					<div className='font-big'>{countdown.hours}</div>
 					<div className='font-small'>HOURS</div>
 				</div>
 				<div className='flex'>
-					<div className='font-big'>13</div>
+					<div className='font-big'>{countdown.minutes}</div>
 					<div className='font-small'>MINUTES</div>
 				</div>
 				<div className='flex'>
-					<div className='font-big'>13</div>
+					<div className='font-big'>{countdown.seconds}</div>
 					<div className='font-small'>SECOND</div>
 				</div>
 			</div>
