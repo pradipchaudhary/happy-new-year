a9006544c56cb783265bc016f5cadc81d0267700
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-04-23 18:00:57 +0545

    update layout and fix sum bug.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,57 @@
 import React, { useEffect, useState } from "react";
 
-const NEW_YEARS = new Date(2023, 3, 14, 0, 0, 0);
+const NEW_YEARS = new Date(2023, 4, 14, 0, 0, 0);
+
+const makeTwoDigitNumber = (number) => {
+	return number > 9 ? number : `0${number}`;
+};
+
+// Convert string to number format
+const stringToNumber = (str) => {
+	const number = parseInt(str, 10);
+	return number;
+};
+
 const getTimedelta = () => {
 	const now = new Date();
 	const timedelta = NEW_YEARS.getTime() - now.getTime();
 
 	// console.log(timedelta);
 	// Diff in days
-	const days = Math.floor(timedelta / (24 * 60 * 60 * 1000));
+	let d = Math.floor(timedelta / (24 * 60 * 60 * 1000));
 
 	// Diff in hours
-	const hours = Math.floor(
-		(timedelta % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000)
-	);
+	let h = Math.floor((timedelta % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
 	// Diff in minutes
-	const minutes = Math.floor((timedelta % (60 * 60 * 1000)) / (60 * 1000));
+	let m = Math.floor((timedelta % (60 * 60 * 1000)) / (60 * 1000));
 
 	// Diffrent in second
-	const seconds = Math.floor((timedelta % (60 * 1000)) / 1000);
+	let s = Math.floor((timedelta % (60 * 1000)) / 1000);
+
+	const days = makeTwoDigitNumber(d);
+	const hours = makeTwoDigitNumber(h);
+	const minutes = makeTwoDigitNumber(m);
+	const seconds = makeTwoDigitNumber(s);
+	console.log("result: ", days, hours, minutes, seconds);
+
+	// console.log(typeof days);
+	// console.log(typeof hours);
+	// console.log(typeof minutes);
+	// console.log(typeof seconds);
+
+	// stringToNumber(days);
+	// stringToNumber(hours);
+	// stringToNumber(minutes);
+	// stringToNumber(seconds);
+	// console.log("result: ", days, hours, minutes, seconds);
+	// console.log(typeof days);
+	// console.log(typeof hours);
+	// console.log(typeof minutes);
+	// console.log(typeof seconds);
+
+	// parseInt(days, 10);
+
+	// console.log("after conversion string to number", days);
 
 	return {
 		days,
